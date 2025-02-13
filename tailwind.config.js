b2cad32017dc4cea64986fb2099d1179d633609c
Luis <luisgoettler@gmail.com>
2025-02-13 21:39:52 +0100

    Improve UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
 				darkprimary: "#303030",
 				lightprimary: "#c4cad4",
 			},
+			keyframes: {
+				fadeIn: {
+					"0%": { opacity: "0" },
+					"100%": { opacity: "1" },
+				},
+			},
+			animation: {
+				fadeIn: "fadeIn 0.3s ease-in-out",
+			},
 		},
 	},
 	plugins: [],
